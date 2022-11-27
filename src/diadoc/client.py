22ba01e1543a8c3119fa7b382758bf7c0215898b
Nikolay Kiryanov <nkiryanov@fands.dev>
2022-11-28 02:23:19 +0300

    DiadocClient: `get_counteragents` method (#10)
    * DiadocClient: `get_my_counteragents` method

--- src/diadoc/client.py
+++ src/diadoc/client.py
@@ -1,5 +1,6 @@
 from diadoc.http import DiadocHTTP
 from diadoc.models import DiadocLegalEntity
+from diadoc.types import DiadocCounteragent
 from diadoc.types import DiadocOrganization
 
 
@@ -10,3 +11,26 @@ class DiadocClient:
     def get_my_organizations(self) -> list[DiadocLegalEntity]:
         organizations: list[DiadocOrganization] = self.http.get("GetMyOrganizations")["Organizations"]  # type: ignore
         return DiadocLegalEntity.from_organization_list(organizations)
+
+    def get_counteragents(self, my_organization_id: str) -> list[DiadocLegalEntity]:
+        """Return organization counteragents from possibly paginated API.
+
+        To get next page:
+            Step 1: Get `IndexKey` from last counteragent in response
+            Step 2: Send request with query param `afterIndexKey` equals `IndexKey` from Step 1
+        """
+        params = {"myOrgId": my_organization_id}
+
+        counteragents: list[DiadocCounteragent] = []
+        paginated_counteragents_empty = False
+
+        while not paginated_counteragents_empty:
+            paginated_counteragents: list[DiadocCounteragent] = self.http.get(url="V2/GetCounteragents", params=params)["Counteragents"]  # type: ignore
+
+            if paginated_counteragents:
+                counteragents += paginated_counteragents
+                params["afterIndexKey"] = paginated_counteragents[-1]["IndexKey"]
+            else:
+                paginated_counteragents_empty = True
+
+        return DiadocLegalEntity.from_counteragents_list(counteragents)
--- src/diadoc/models.py
+++ src/diadoc/models.py
@@ -1,7 +1,27 @@
 from dataclasses import dataclass
+from enum import Enum
 
 from app.models import LegalEntity
+from diadoc.types import DiadocCounteragent
 from diadoc.types import DiadocOrganization
+
+
+class PartnershipStatus(Enum):
+    ESTABLISHED = "established"
+    INVITE_SHOULD_BE_SEND = "invite_should_be_send"
+    INVITE_WAS_SENT = "invite_was_sent"
+    REJECTED = "rejected"
+
+
+COUNTERAGENT_PARTNERSHIP_STATUS_MAP = {
+    "UnknownCounteragentStatus": PartnershipStatus.INVITE_SHOULD_BE_SEND,
+    "IsMyCounteragent": PartnershipStatus.ESTABLISHED,
+    "InvitesMe": PartnershipStatus.INVITE_SHOULD_BE_SEND,
+    "IsInvitedByMe": PartnershipStatus.INVITE_WAS_SENT,
+    "RejectsMe": PartnershipStatus.REJECTED,
+    "IsRejectedByMe": PartnershipStatus.REJECTED,
+    "NotInCounteragentList": PartnershipStatus.INVITE_SHOULD_BE_SEND,
+}
 
 
 @dataclass
@@ -9,6 +29,7 @@ class DiadocLegalEntity(LegalEntity):
     diadoc_id: str
     is_active: bool
     is_roaming: bool
+    diadoc_partnership_status: PartnershipStatus | None = None
 
     @staticmethod
     def from_organization(organization=DiadocOrganization) -> "DiadocLegalEntity":
@@ -24,3 +45,13 @@ class DiadocLegalEntity(LegalEntity):
     @classmethod
     def from_organization_list(cls, organizations: list[DiadocOrganization]) -> list["DiadocLegalEntity"]:
         return [cls.from_organization(organization) for organization in organizations]
+
+    @classmethod
+    def from_counteragent(cls, counteragent=DiadocCounteragent) -> "DiadocLegalEntity":
+        legal_entity = cls.from_organization(counteragent["Organization"])
+        legal_entity.diadoc_partnership_status = COUNTERAGENT_PARTNERSHIP_STATUS_MAP[counteragent["CurrentStatus"]]
+        return legal_entity
+
+    @classmethod
+    def from_counteragents_list(cls, counteragents: list[DiadocCounteragent]) -> list["DiadocLegalEntity"]:
+        return [cls.from_counteragent(counteragent) for counteragent in counteragents]
