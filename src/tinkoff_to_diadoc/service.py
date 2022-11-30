425820b8bcc5b04a4f429385448f9d8b5584d474
Nikolay Kiryanov <nikolay@kiryanov.ru>
2022-11-30 14:28:04 +0300

    `my_company`, `bank_accounts` are properties now

--- src/tinkoff_to_diadoc/service.py
+++ src/tinkoff_to_diadoc/service.py
@@ -1,3 +1,4 @@
+from functools import cached_property
 import os
 
 from app.models import BankAccount
@@ -14,7 +15,8 @@ class TinkoffToDiadoc:
         self.diadoc = DiadocClient()
         self.message_to_acquire = os.getenv("MESSAGE_TO_ACQUIRE_COUNTERAGENT")
 
-    def get_my_company(self) -> DiadocLegalEntity:
+    @cached_property
+    def my_company(self) -> DiadocLegalEntity:
         tinkoff_entity = self.tinkoff.get_company()
         diadoc_entities = self.diadoc.get_my_organizations()
 
@@ -24,8 +26,12 @@ class TinkoffToDiadoc:
 
         raise TinkoffToDiadocException("Did not find in Diadoc's my organizations same company as in Tinkoff")
 
-    def get_bank_accounts(self) -> list[BankAccount]:
+    @property
+    def bank_accounts(self) -> list[BankAccount]:
         return self.tinkoff.get_bank_accounts()
+
+    def get_counteragents(self, my_company: DiadocLegalEntity) -> list[DiadocLegalEntity]:
+        return self.diadoc.get_counteragents(my_company.diadoc_id)
 
     def get_recent_distinct_payers(self, bank_accounts: list[BankAccount], my_company: DiadocLegalEntity) -> list[LegalEntity]:
         payers: list[LegalEntity] = []
@@ -41,9 +47,6 @@ class TinkoffToDiadoc:
                 payers_inn_kpp.add(payer.inn_kpp)
 
         return distinct_payers
-
-    def get_counteragents(self, my_company: DiadocLegalEntity) -> list[DiadocLegalEntity]:
-        return self.diadoc.get_counteragents(my_company.diadoc_id)
 
     def exclude_payers_in_partners(self, payers: list[LegalEntity], counteragents: list[DiadocLegalEntity]) -> list[LegalEntity]:
         partners_already_inn_kpp = {counteragent.inn_kpp for counteragent in counteragents if counteragent.in_partners}
@@ -73,11 +76,9 @@ class TinkoffToDiadoc:
             self.diadoc.acquire_counteragent(diadoc_id=entity.diadoc_id, message=self.message_to_acquire)
 
     def act(self) -> None:
-        my_company = self.get_my_company()
-        bank_accounts = self.get_bank_accounts()
-        recent_payers = self.get_recent_distinct_payers(bank_accounts, my_company)
-        counteragents = self.get_counteragents(my_company)
+        counteragents = self.get_counteragents(self.my_company)
 
+        recent_payers = self.get_recent_distinct_payers(self.bank_accounts, self.my_company)
         payers_not_in_partners = self.exclude_payers_in_partners(recent_payers, counteragents)
 
         entities_from_payers = self.get_diadoc_entities_from_payers(payers_not_in_partners)
--- src/tinkoff_to_diadoc/tests/tests_tinkoff_to_diadoc_propery_bank_accounts.py
+++ src/tinkoff_to_diadoc/tests/tests_tinkoff_to_diadoc_propery_bank_accounts.py
@@ -7,12 +7,12 @@ def mock_tinkoff_get_bank_accounts(mocker, bank_account, ya_bank_account):
 
 
 def test_return_bank_accounts(tinkoff_to_diadoc, bank_account, ya_bank_account):
-    got = tinkoff_to_diadoc.get_bank_accounts()
+    got = tinkoff_to_diadoc.bank_accounts
 
     assert got == [bank_account, ya_bank_account]
 
 
 def test_tinkoff_client_was_called(tinkoff_to_diadoc, mock_tinkoff_get_bank_accounts):
-    tinkoff_to_diadoc.get_bank_accounts()
+    tinkoff_to_diadoc.bank_accounts
 
     mock_tinkoff_get_bank_accounts.assert_called_once()
