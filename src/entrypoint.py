7d83e133c31699c06d6ae5a24b9f1eaf3b06dc9e
Nikolay Kiryanov <nikolay@kiryanov.ru>
2022-12-05 12:55:28 +0300

    Entrypoint to send invites on regular-basis: 1. Entrypoint to run task once a day 2. Dependencies: update and drop unnecessary 3. README updated with base description how it works

---
+++ src/entrypoint.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+import schedule
+
+from app.sentry import init_sentry
+from tinkoff_to_diadoc import TinkoffToDiadoc
+
+
+def send_invites_to_diadoc() -> None:
+    service = TinkoffToDiadoc()
+    service.act()
+
+
+if __name__ == "__main__":
+    init_sentry()
+
+    send_invites_to_diadoc()
+    schedule.every().day.at(os.getenv("AT", "09:30")).do(send_invites_to_diadoc)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
